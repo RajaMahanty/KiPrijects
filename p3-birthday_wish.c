6835d0f234411571f0230c27c8da3c7a59cb7281
RajaMahanty <94551338+RajaMahanty@users.noreply.github.com>
2021-12-08 20:03:20 +0530

    Create p3-birthday_wish.c

---
+++ p3-birthday_wish.c
@@ -0,0 +1,150 @@
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(){
+    char name[50];
+    int hour, minute, second;
+    int HH,MM,SS;
+ 
+    hour = minute = second = 0;
+    
+
+   	printf("\n\n");
+   	
+   			          
+        printf("\tEnter Friend Name  : ");
+         
+        scanf("%s",name); 
+                 
+        printf("\n===================================\n");
+		  
+        printf("\tEnter Wishing Time  \n");
+        printf("\n===================================\n");
+        printf("\nHH:MM:SS\n");
+        scanf("%d %d %d",&hour,&minute,&second);
+        
+        
+    while(1){
+    
+    system("CLS");    	
+        
+
+        printf("\n\n\n\n\n\n");
+        printf("\t\t\t\t\t\t\t");
+        printf("HH : MM : SS\n\t\t\t\t\t\t\t");
+        printf("%.2d : %.2d : %.2d", hour, minute, second);
+      
+        second++;
+
+        if(second==60)
+		{
+            minute = minute + 1;
+        
+        }
+
+        if(minute == 60)
+		{
+
+            hour = hour + 1;
+        }
+
+        if(second == 60 )
+		{
+            second = 0;
+        }
+         if(minute == 60 )
+		{
+            minute = 0;
+        }
+         if(hour == 12 )
+		{
+            hour = 0;
+        }
+       
+        sleep(1);
+		
+		if(hour==00&&minute==00&&second==05)
+		
+		
+{
+	
+	printf("\n=================================================================================================================\n\n");
+	printf("\n\t\t\tGOD BLESS YOU & ALL YOUR DESIRES COMES TRUE\n");
+	printf("\n\t\t\t  MANY MANY HAPPY RETURNS OF THE DAY\n");
+	printf("\n\t\t\t\tENJOY YOUR SPECIAL DAY\t");puts(name);
+	printf("\n=================================================================================================================\n");
+	sleep(2);	
+		
+printf("\n\n\n\n");
+printf("\nHH         HH    AAAAAAAAA     PPPPPPPPP     PPPPPPPPP     YY         YY");
+printf("\nHH         HH   AA       AA   PP       PP   PP       PP     YY      YY");
+printf("\nHH         HH   AA       AA   PP       PP   PP       PP       YY  YY");
+printf("\nHHHHHHHHHHHHH   AAAAAAAAAAA   PPPPPPPPPPP   PPPPPPPPPPP         YY  ");
+printf("\nHH         HH   AA       AA   PP            PP                  Y");
+printf("\nHH         HH   AA       AA   PP            PP                  Y");
+printf("\nHH         HH   AA       AA   PP            PP                  Y");
+sleep(1);
+
+
+
+printf("\n\n");
+printf("\nBBBBBBBBBBB     IIIIIIIIIIIII    RRRRRRRRRRR   TTTTTTTTTTTTTTTT      HH         HH");
+printf("\nBB         BB        II          RR        RR         TT             HH         HH");
+printf("\nBB         BB        II          RR        RR         TT             HH         HH");
+printf("\nBBBBBBBBBBB          II          RRRRRRRRRRR          TT             HHHHHHHHHHHHH");
+printf("\nBB         BB        II          RR RRR               TT             HH         HH");
+printf("\nBB         BB        II          RR    RR             TT             HH         HH");
+printf("\nBBBBBBBBBBB     IIIIIIIIIIIII    RR       RR          TT             HH         HH");
+sleep(1);
+	
+
+      printf("\n\n\t\n\t\t");
+printf("\n DDDDDDDDD    \t AAAAA \t YY      YY");
+printf("\n   DD     DD\tAA   AA	   YY   YY    ");
+printf("\n   DD     DD\tAA   AA	     YY       ");
+printf("\n   DD     DD\tAAAAAAA	     Y        ");
+printf("\n   DD     DD\tAA   AA      Y        ");
+printf("\n DDDDDDDDD  \tAA   AA	     Y        " )   ;
+sleep(1);
+
+printf("\t\n\t\t\t\t\t\t\t\t    Dear   %s         \n",name);
+printf("\t\n\t\t\t\t\t\t\t\t   Make every day of your life and  \n");
+printf("\t\n\t\t\t\t\t\t\t\t   every candle count . you are worth it .\n");
+printf("\t\n\t\t\t\t\t\t\t\t  Happiest of birthdays to you .\n");
+printf("\t\n\t\t\t\t\t\t\t\t   best  year  ever !\n");
+sleep(1);
+
+
+printf("\t\n\t\t\t\t\t\t\t\t                 ^        ");
+printf("\t\n\t\t\t\t\t\t\t\t                | |        ");
+printf("\t\n\t\t\t\t\t\t\t\t           =============     ");
+printf("\t\n\t\t\t\t\t\t\t\t           |   HAPPY   |        ");
+printf("\t\n\t\t\t\t\t\t\t\t         ==================     ");
+printf("\t\n\t\t\t\t\t\t\t\t         |     BIRTH      |");
+printf("\t\n\t\t\t\t\t\t\t\t       =====================     ");
+printf("\t\n\t\t\t\t\t\t\t\t       |       DAY        |    ");
+printf("\t\n\t\t\t\t\t\t\t\t    ==========================");
+printf("\t\n\t\t\t\t\t\t\t\t    |          %s            |",name);
+printf("\t\n\t\t\t\t\t\t\t\t   =============================\n\n");
+sleep(2);
+ 
+
+printf("\t\n\t^" "\t^"  "\t^" "\t^"  "\t^" "\t^"  "\t^" "\t^"  "\t^" "\t^"  "\t^" "\t^"  "\t^" "\t^"  "\t^" );
+printf("\t\n\t==" "\t==" "\t==" "\t==" "\t==" "\t==" "\t==" "\t==" "\t==" "\t==" "\t==" "\t==" "\t==" "\t==");
+printf("\t\n\t==" "\t==" "\t==" "\t==" "\t==" "\t==" "\t==" "\t==" "\t==" "\t==" "\t==" "\t==" "\t==" "\t==");
+printf("\t\n\t==" "\t==" "\t==" "\t==" "\t==" "\t==" "\t==" "\t==" "\t==" "\t==" "\t==" "\t==" "\t==" "\t==");
+printf("\t\n\t==" "\t==" "\t==" "\t==" "\t==" "\t==" "\t==" "\t==" "\t==" "\t==" "\t==" "\t==" "\t==" "\t==");
+printf("\t\n\t==" "\t==" "\t==" "\t==" "\t==" "\t==" "\t==" "\t==" "\t==" "\t==" "\t==" "\t==" "\t==" "\t==");
+printf("\t\n\t==" "\t==" "\t==" "\t==" "\t==" "\t==" "\t==" "\t==" "\t==" "\t==" "\t==" "\t==" "\t==" "\t==");
+sleep(1);
+
+printf("\n  ==================================================================================================================\n");
+printf("\n  ==================================================================================================================\n");
+break;
+
+}
+}
+    return 0;
+}
